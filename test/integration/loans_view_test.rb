d2da9bc9a0dc3a2b66219b61599528f541328b10
_godiah_ <51189464+godiah@users.noreply.github.com>
2025-02-12 14:11:15 +0300

    Update loans_view_test.rb

--- test/integration/loans_view_test.rb
+++ test/integration/loans_view_test.rb
@@ -4,7 +4,6 @@ class LoansViewTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:john)
     post sign_in_path, params: { email: @user.email, password: "password" }
-    # Ensure at least one loan exists for the user
     @loan = Loan.create!(user: @user, book: books(:moby_dick), borrowed_at: Time.current, due_date: Time.current + 2.weeks)
   end
 
@@ -13,7 +12,6 @@ class LoansViewTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "h2", "My Borrowed Books"
     assert_match @loan.book.title, response.body
-    # Verify the due date is formatted as expected (example: "Due Date: May 15, 2025")
     assert_select "p", /Due Date:/
   end
 end
